8061d097e16e1864b94b030d1a803460d8623d2e
Jonathancollinet <jonathan.collinet@epitech.eu>
2015-03-05 18:19:34 +0100

    validation flux ok -> lien vers validation d'une proposition

--- public/views/events/validations/index.js
+++ public/views/events/validations/index.js
@@ -6,8 +6,11 @@
   app.FluxValidationModel = Backbone.Model.extend({
     idAttribute: '_id',
   	defaults: {
-  		epicture: '',
-  		eusername: ''
+  		_id: '',
+  		e: '',
+  		picture: '',
+  		acc: '',
+  		isValidate: false
   	}
   });
 
@@ -26,15 +29,19 @@
   	},
   	initialize: function(item) {
   		this.model = new app.FluxValidationModel();
-      this.model.set(item);
-  		console.log("flux validation ok");
-  		this.render();
+      var i = 0;
+      console.log(item);
+      while (i < item.length) {
+	      this.model.set(item[i]);
+	      console.log(this.model.attributes);
+	  		this.render();
+      	++i;
+      }
   	},
   	visualizer: function(e) {
   	},
   	render: function() {
-      this.$el.html(this.template( this.model.attributes ));
-      console.log("valiflu render ok");
+      this.$el.html(this.$el.html() + this.template( this.model.attributes ));
   	}
   });
 
@@ -43,13 +50,10 @@
     template: _.template( $('#tmpl-legend').html() ),
   	initialize: function(item) {
   		this.model = new app.LegendModel();
-      this.model.set(item);
-  		console.log("legend ok");
   		this.render();
   	},
   	render: function() {
-      this.$el.html(this.template( this.model.attributes ));
-  		console.log("legend render ok");
+      // this.$el.html(this.template( this.model.attributes ));
   	}
   });
 
@@ -63,8 +67,19 @@
   });
 
   $(document).ready(function(){
-  	app.FluxValidation = new app.FluxValidationView(JSON.parse( unescape($('#data-valiflux').html()) ));
-  	app.Legend = new app.LegendView(JSON.parse( unescape($('#data-legend').html()) ));
+
+  	var val = JSON.parse( unescape($('#data-valiflux').html()) );
+  	var i = 0;
+
+  	app.FlucValidation = new app.FluxValidationView(val);
+  	app.Legend = new app.LegendView(val);
+
+
+		if ($(".picture").attr("src") != "")
+			$("#no_result").hide();
+		else
+			$(".item").hide();
+
   });
 
 }());
