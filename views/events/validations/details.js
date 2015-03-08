a9c091320f3bc92c53c5355c084760ad073a33bb
Jonathancollinet <jonathan.collinet@epitech.eu>
2015-03-08 16:05:26 +0100

    change visualizer => details

--- views/events/validations/details.js
+++ views/events/validations/details.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 exports.init = function(req, res, next) {
 	req.app.db.models.Validation.findById(req.params.vid).populate("eid uid erid").exec(function(err, row) {
 		if (!err) {
-			res.render('events/validations/visualizer', {row: escape(JSON.stringify(row))});
+			res.render('events/validations/details', {row: escape(JSON.stringify(row))});
 		}
 	});
 }
